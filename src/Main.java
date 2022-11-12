a6f417fc88bd8064335f84e831012480a9e7a48d
Nicola-Matis <114828368+Nicola-Matis@users.noreply.github.com>
2022-11-12 15:41:36 +0200

    Update Main.java
    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LogicalOp logicalOp = new LogicalOp();
+        System.out.println("Exercitiul 1");
+        logicalOp.Ex1(90);
+        System.out.println("Exercitiul 2");
+        logicalOp.Ex2(-90);
+        System.out.println("Exercitiul 3");
+        logicalOp.Ex3(80,90);
+        System.out.println("Exercitiul 4");
+        logicalOp.Ex4(80,50);
+        System.out.println("Exercitiul 5");
+        logicalOp.Ex5();
+        System.out.println("Exercitiul 6");
+        logicalOp.Ex6();
+        System.out.println("Exercitiul 7");
+        System.out.println(logicalOp.Ex7(111,8899));
+        System.out.println("Exercitiul 8");
+        System.out.println(logicalOp.Ex8(6,40));
+        System.out.println("Exercitiul 9");
+        logicalOp.Ex9();
     }
 }
